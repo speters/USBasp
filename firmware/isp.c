0de29c22270d35b44a208637e80a00828bedb8c8
Soenke J. Peters <peters__git@opcenter.de>
2014-01-16 17:36:14 +0100

    Add AT89Sxx support from http://rgwan.byethost10.com/archives/1328

--- firmware/isp.c
+++ firmware/isp.c
@@ -7,22 +7,27 @@
  * Licence........: GNU GPL v2 (see Readme.txt)
  * Creation Date..: 2005-02-23
  * Last change....: 2009-02-28
+ * AVR芯片已经支持
+ * AT89S系列完美支持
+ * 就差LGT芯片
  */
 
-#include <avr/io.h>
+#include <avr>
 #include "isp.h"
 #include "clock.h"
 #include "usbasp.h"
 
-#define spiHWdisable() SPCR = 0
+#define spiHWdisable() UCSR0B=0x00
 
-uchar sck_sw_delay;
-uchar sck_spcr;
-uchar sck_spsr;
-
+uchar sck_sw_delay;//实际上软件SPI已经不存在
+uchar sck_ubrrh;
+uchar sck_ubrrl;//SPI速率
+uchar chip_type;//芯片型号 0代表AVR,1代表S5x,2代表LGT8F系列
 void spiHWenable() {
-	SPCR = sck_spcr;
-	SPSR = sck_spsr;
+	UBRR0H=sck_ubrrh;
+	UBRR0L=sck_ubrrl;
+	UCSR0B= _BV(TXEN) | _BV(RXEN);//允许发送和接收
+	UCSR0C= _BV(UMSEL1) | _BV(UMSEL0);//在CLK的上升沿接收、下降沿输出,SPI主机模式
 }
 
 void ispSetSCKOption(uchar option) {
@@ -30,164 +35,182 @@ void ispSetSCKOption(uchar option) {
 	if (option == USBASP_ISP_SCK_AUTO)
 		option = USBASP_ISP_SCK_375;
 
-	if (option >= USBASP_ISP_SCK_93_75) {
-		ispTransmit = ispTransmit_hw;
-		sck_spsr = 0;
-
+	//if (option >= USBASP_ISP_SCK_93_75) {
+	//	ispTransmit = ispTransmit_hw;
+		sck_sw_delay = 3;
 		switch (option) {
-
+		//大改……
+		case USBASP_ISP_SCK_6000:
+			sck_ubrrh=0;
+			sck_ubrrl=0x00;//6Mbps
+		case USBASP_ISP_SCK_3000:
+			sck_ubrrh=0;
+			sck_ubrrl=0x01;//3Mbps		
+			break;
 		case USBASP_ISP_SCK_1500:
 			/* enable SPI, master, 1.5MHz, XTAL/8 */
-			sck_spcr = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
-			sck_spsr = (1 << SPI2X);
+			sck_ubrrh=0;
+			sck_ubrrl=0x03;//1.5Mbps
+			break;
 		case USBASP_ISP_SCK_750:
 			/* enable SPI, master, 750kHz, XTAL/16 */
-			sck_spcr = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+			sck_ubrrh=0;
+			sck_ubrrl=0x07;
 			break;
 		case USBASP_ISP_SCK_375:
 		default:
 			/* enable SPI, master, 375kHz, XTAL/32 (default) */
-			sck_spcr = (1 << SPE) | (1 << MSTR) | (1 << SPR1);
-			sck_spsr = (1 << SPI2X);
+			sck_ubrrh=0;
+			sck_ubrrl=15;//375K
 			break;
 		case USBASP_ISP_SCK_187_5:
 			/* enable SPI, master, 187.5kHz XTAL/64 */
-			sck_spcr = (1 << SPE) | (1 << MSTR) | (1 << SPR1);
+			sck_ubrrh=0;
+			sck_ubrrl=31;
 			break;
 		case USBASP_ISP_SCK_93_75:
 			/* enable SPI, master, 93.75kHz XTAL/128 */
-			sck_spcr = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
+			sck_ubrrh=0;
+			sck_ubrrl=63;
 			break;
-		}
-
-	} else {
-		ispTransmit = ispTransmit_sw;
-		switch (option) {
 
 		case USBASP_ISP_SCK_32:
-			sck_sw_delay = 3;
+			sck_ubrrh=0;
+			sck_ubrrl=0xb9;
 
 			break;
 		case USBASP_ISP_SCK_16:
-			sck_sw_delay = 6;
+			sck_ubrrh=1;
+			sck_ubrrl=0x72;
 
 			break;
 		case USBASP_ISP_SCK_8:
-			sck_sw_delay = 12;
+			sck_ubrrh=2;
+			sck_ubrrl=0xe4;
 
 			break;
 		case USBASP_ISP_SCK_4:
-			sck_sw_delay = 24;
+			sck_ubrrh=5;
+			sck_ubrrl=0xc8;
 
 			break;
 		case USBASP_ISP_SCK_2:
-			sck_sw_delay = 48;
+			sck_ubrrh=0xb;
+			sck_ubrrl=0x90;
 
 			break;
 		case USBASP_ISP_SCK_1:
-			sck_sw_delay = 96;
+			sck_ubrrh=0x17;
+			sck_ubrrl=0x20;
 
 			break;
 		case USBASP_ISP_SCK_0_5:
-			sck_sw_delay = 192;
+			sck_ubrrh=0x2e;
+			sck_ubrrl=0x40;
 
 			break;
 		}
-	}
 }
 
 void ispDelay() {
 
 	uint8_t starttime = TIMERVALUE;
-	while ((uint8_t) (TIMERVALUE - starttime) < sck_sw_delay) {
+	if(chip_type=CHIP_S5x)
+	{
+		while ((uint8_t) (TIMERVALUE - starttime) < 4*sck_sw_delay) {
+		}
 	}
+	else
+	{
+		while ((uint8_t) (TIMERVALUE - starttime) < 2*sck_sw_delay) {
+		}
+	}	
 }
 
-void ispConnect() {
+void ispReset()
+{
 
+	if(chip_type == CHIP_AVR)
+	{
+		ISP_OUT &= ~(1 << ISP_RST); //AVR负脉冲复位
+
+	}
+	else if(chip_type == CHIP_S5x)
+	{
+		ISP_OUT |= (1 << ISP_RST); /* RST high */
+	}
+	ispDelay();
+	ispDelay();
+	ispDelay();
+	ispDelay();//等待让芯片内部复位
+}
+void ispConnect() {
+	
 	/* all ISP pins are inputs before */
 	/* now set output pins */
-	ISP_DDR |= (1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI);
-
+	//SPI_DDR &= ~(1<< ISP_MISO);
+	CK_OUT &= ~(1<< ISP_SCK);
+	ISP_DDR |= (1 << ISP_RST) ;
+	CK_DDR  |= (1 << ISP_SCK);
+	SPI_DDR |= (1<< ISP_MOSI);
+	//if (ispTransmit == ispTransmit_hw) {
+		spiHWenable();//SPI开
+	//}
 	/* reset device */
+	CK_OUT &= ~(1 << ISP_SCK); /* SCK low */
 	ISP_OUT &= ~(1 << ISP_RST); /* RST low */
-	ISP_OUT &= ~(1 << ISP_SCK); /* SCK low */
 
 	/* positive reset pulse > 2 SCK (target) */
 	ispDelay();
 	ISP_OUT |= (1 << ISP_RST); /* RST high */
-	ispDelay();
-	ISP_OUT &= ~(1 << ISP_RST); /* RST low */
-
-	if (ispTransmit == ispTransmit_hw) {
-		spiHWenable();
-	}
+	
+	ispReset();
 }
+
 
 void ispDisconnect() {
-
-	/* set all ISP pins inputs */
-	ISP_DDR &= ~((1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI));
-	/* switch pullups off */
-	ISP_OUT &= ~((1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI));
-
-	/* disable hardware SPI */
 	spiHWdisable();
-}
+	/* set all ISP pins inputs */
+	ISP_DDR &= ~(1 << ISP_RST);// | (1 << ISP_SCK) | (1 << ISP_MOSI));
+	SPI_DDR &= ~(1 << ISP_MOSI) ;
+	SPI_DDR &= ~(1 << ISP_MISO);//关闭输出
+	CK_DDR &= ~(1 << ISP_SCK);
+	/* switch pullups off */
+	ISP_OUT &= ~(1 << ISP_RST);// | (1 << ISP_SCK) | (1 << ISP_MOSI));
+	SPI_OUT &= ~(1 << ISP_MOSI);
+	SPI_OUT &= ~(1 << ISP_MISO);
+	CK_OUT &= ~(1 << ISP_SCK);//关闭上拉
+	/* disable hardware SPI */
 
-uchar ispTransmit_sw(uchar send_byte) {
-
-	uchar rec_byte = 0;
-	uchar i;
-	for (i = 0; i < 8; i++) {
-
-		/* set MSB to MOSI-pin */
-		if ((send_byte & 0x80) != 0) {
-			ISP_OUT |= (1 << ISP_MOSI); /* MOSI high */
-		} else {
-			ISP_OUT &= ~(1 << ISP_MOSI); /* MOSI low */
-		}
-		/* shift to next bit */
-		send_byte = send_byte << 1;
-
-		/* receive data */
-		rec_byte = rec_byte << 1;
-		if ((ISP_IN & (1 << ISP_MISO)) != 0) {
-			rec_byte++;
-		}
-
-		/* pulse SCK */
-		ISP_OUT |= (1 << ISP_SCK); /* SCK high */
-		ispDelay();
-		ISP_OUT &= ~(1 << ISP_SCK); /* SCK low */
-		ispDelay();
-	}
-
-	return rec_byte;
 }
 
 uchar ispTransmit_hw(uchar send_byte) {
-	SPDR = send_byte;
+	UDR0 = send_byte;
 
-	while (!(SPSR & (1 << SPIF)))
-		;
-	return SPDR;
+	while (!(UCSR0A & _BV(UDRE)));//等待数据寄存器空
+	//while (!(UCSR0A & _BV(TXC)));
+	while (!(UCSR0A & _BV(RXC)));//等待接收完成
+	return UDR0;
 }
 
-uchar ispEnterProgrammingMode() {
-	uchar check;
-	uchar count = 32;
-
+uchar checkOut()
+{
+	uchar check,check1;
+	uchar count ;
+	count=16;//尝试16次
+	ispReset();
 	while (count--) {
 		ispTransmit(0xAC);
 		ispTransmit(0x53);
 		check = ispTransmit(0);
-		ispTransmit(0);
+		check1= ispTransmit(0);//89S52
 
 		if (check == 0x53) {
-			return 0;
+			return 0; //三字节0x53是AVR
 		}
-
+		if(check1 == 0x69 && chip_type == CHIP_S5x){
+			return 0;//四字节0x69是89S52
+		}    
 		spiHWdisable();
 
 		/* pulse SCK */
@@ -196,15 +219,24 @@ uchar ispEnterProgrammingMode() {
 		ISP_OUT &= ~(1 << ISP_SCK); /* SCK low */
 		ispDelay();
 
-		if (ispTransmit == ispTransmit_hw) {
+		//if (ispTransmit == ispTransmit_hw) {
 			spiHWenable();
-		}
+		//}
 
 	}
-
-	return 1; /* error: device dosn't answer */
+	return 1;
 }
+uchar ispEnterProgrammingMode() {
 
+	
+	chip_type=CHIP_AVR;//先用AVR试验
+	
+	if(checkOut() ==0) return 0;
+	chip_type=CHIP_S5x;
+	if(checkOut() ==0) return 0;
+	return 1;
+
+}
 uchar ispReadFlash(unsigned long address) {
 	ispTransmit(0x20 | ((address & 1) << 3));
 	ispTransmit(address >> 9);
@@ -213,7 +245,7 @@ uchar ispReadFlash(unsigned long address) {
 }
 
 uchar ispWriteFlash(unsigned long address, uchar data, uchar pollmode) {
-
+	uchar retries ;
 	/* 0xFF is value after chip erase, so skip programming
 	 if (data == 0xFF) {
 	 return 0;
@@ -234,7 +266,8 @@ uchar ispWriteFlash(unsigned long address, uchar data, uchar pollmode) {
 	} else {
 
 		/* polling flash */
-		uchar retries = 30;
+		if(chip_type==CHIP_AVR) retries=30;
+		if(chip_type == CHIP_S5x) retries=50;
 		uint8_t starttime = TIMERVALUE;
 		while (retries != 0) {
 			if (ispReadFlash(address) != 0x7F) {
@@ -291,7 +324,8 @@ uchar ispReadEEPROM(unsigned int address) {
 	return ispTransmit(0);
 }
 
-uchar ispWriteEEPROM(unsigned int address, uchar data) {
+uchar ispWriteEEPROM(unsigned int address, uchar data) 
+{
 
 	ispTransmit(0xC0);
 	ispTransmit(address >> 8);
@@ -302,3 +336,4 @@ uchar ispWriteEEPROM(unsigned int address, uchar data) {
 
 	return 0;
 }
+</avr></tfischl@gmx.de>
